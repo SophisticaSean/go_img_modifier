17cdb93824ab64d5057728b2264361d5b891928d
Sean Lewis <sensaisean@gmail.com>
2016-08-27 03:18:01 -0600

    now complete with image compositing

---
+++ README.md
@@ -0,0 +1,5 @@
+alternative to i3fancylock
+
+usage:
+  `go_img_modifier source_img dest_img_path blur/pixelate_amount pixel/blur_option optional:image_to_put_in_center_of_background`
+  `go_img_modifier screenshot.png final.png 10 pixel lock.png`
--- main.go
+++ main.go
@@ -10,14 +10,25 @@ import (
 
 func main() {
 	img, _ := imaging.Open(os.Args[1])
+	bigImg := img
 	max := img.Bounds().Max
 	var min image.Point
+	var mid image.Point
 	blur_amount, _ := strconv.Atoi(os.Args[3])
 	min.X = max.X / blur_amount
 	min.Y = max.Y / blur_amount
+	mid.X = max.X / 2
+	mid.Y = max.Y / 2
 
-	//smallImg := imaging.Blur(img, 20)
-	smallImg := imaging.Resize(img, min.X, min.Y, imaging.NearestNeighbor)
-	bigImg := imaging.Resize(smallImg, max.X, max.Y, imaging.NearestNeighbor)
+	if (os.Args[4] == "blur") {
+		bigImg = imaging.Blur(img, float64(blur_amount))
+	} else {
+		smallImg := imaging.Resize(img, min.X, min.Y, imaging.NearestNeighbor)
+		bigImg = imaging.Resize(smallImg, max.X, max.Y, imaging.NearestNeighbor)
+	}
+	if (len(os.Args) == 6) {
+		lock, _ := imaging.Open(os.Args[5])
+		bigImg = imaging.OverlayCenter(bigImg, lock, 1.0)
+	}
 	imaging.Save(bigImg, os.Args[2])
 }
